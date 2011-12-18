0d73b468aa7c6c6f681a9411c40f8ce9a68bca40
unknown <Raja Mohamed@RajaMohamed.(none)>
2011-12-18 06:56:07 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projects24x7::Application.config.secret_token = 'e1b7297f1013ba494cb2f2b4ea837c90e71ff7f68bd3dd84b773af05f2fc6a677c50fdc119daf1ee7b1bf8e340aed60b7ecc822e58d07d0b4dbd4625ca32e4c1'
